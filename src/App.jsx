c91cbfc4b5943955fd3370c009d578e989e58320
César Martinez <9775263+cermartinezz@users.noreply.github.com>
2024-07-25 23:08:11 -0600

    use useRef to control first input

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,53 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
-export const App = () => {
-  const { movies } = useMovies()
-  const [query, setQuery] = useState('')
+function useSearch () {
+  const [search, setSearch] = useState('')
   const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    console.log(query)
-  }
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
 
-  const handleOnChange = (event) => {
-    const value = event.target.value
-    setQuery(value)
-
-    if (value === '') {
+    if (search === '') {
       setError('Can not search movies with empty values')
       return
     }
 
-    if (value.match(/^\d+$/)) {
+    if (search.match(/^\d+$/)) {
       setError('Search must have letters')
       return
     }
 
-    if (value.length < 3) {
-      setError('Search must have more than 3 characters')
+    if (search.length < 3) {
+      setError('Search must have more than 3 charactes')
       return
     }
 
     setError(null)
+  }, [search])
+
+  return { search, setSearch, error }
+}
+
+export const App = () => {
+  const { movies } = useMovies()
+  const { search, setSearch, error } = useSearch()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(search)
+  }
+
+  const handleChange = (event) => {
+    const value = event.target.value
+
+    setSearch(value)
   }
 
   return (
@@ -40,10 +55,10 @@ export const App = () => {
       <header>
         <h1>Search movies</h1>
         <form className='form' onSubmit={handleSubmit}>
-          <input onChange={handleOnChange} name='query' type='text' placeholder='Avengers, Inside Out 2, ...' />
+          <input onChange={handleChange} name='query' type='text' placeholder='Avengers, Inside Out 2, ...' />
           <button>Search</button>
         </form>
-        {error && <p>{error}</p>}
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
       <main>
         <Movies movies={movies} />

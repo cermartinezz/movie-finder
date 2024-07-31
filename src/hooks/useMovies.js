3db00d0210278e6c0da09391c8498b775e999c52
César Martinez <9775263+cermartinezz@users.noreply.github.com>
2024-07-30 22:18:07 -0600

    change useMemo for useCallback

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,4 +1,4 @@
-import { useMemo, useRef, useState } from 'react'
+import { useCallback, useMemo, useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
 
 export function useMovies ({ search, sort }) {
@@ -7,23 +7,21 @@ export function useMovies ({ search, sort }) {
   const [error, setError] = useState(null)
   const previousSearch = useRef(search)
 
-  const getMovies = useMemo(() => {
-    return async ({ search }) => {
-      if (search === previousSearch.current) return
+  const getMovies = useCallback(async ({ search }) => {
+    if (search === previousSearch.current) return
 
-      try {
-        setLoading(true)
-        setError(null)
-        previousSearch.current = search
-        const newMovies = await searchMovies({ search })
-        setMovies(newMovies)
-      } catch (e) {
-        setError(e.message)
-      } finally {
-        setLoading(false)
-      }
+    try {
+      setLoading(true)
+      setError(null)
+      previousSearch.current = search
+      const newMovies = await searchMovies({ search })
+      setMovies(newMovies)
+    } catch (e) {
+      setError(e.message)
+    } finally {
+      setLoading(false)
     }
-  }, [])
+  })
 
   // the logic to sort movies will be store and executed only when movies and sort
   // this is commonly use on big set of data
